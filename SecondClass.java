fb3bd6c151bb8062dad317ccf9032c6afebd45c9
Pawan Rathee <ratheepawan666@gmail.com>
2025-08-01 14:06:58 +0530

    In Today Lecture Learn How to Take input and print output in Java

---
+++ SecondClass.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+
+class SecondClass{
+
+    public static void main(String[] arg){
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+
+        System.out.println(sum);
+
+    }
+}
